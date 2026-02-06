624e509fa1145a35e4b55d0922316670af686d58
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-07 00:52:07 +0600

    jobdetails

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,6 +30,13 @@ async function run() {
     app.get('/jobs',async(req,res)=>{
         const result = await jobCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get("/jobs/:id",async(req,res)=>{
+         const id = req.params.id;
+         const qurey = {_id : new ObjectId(id)};
+         const result = await jobCollection.findOne(qurey);
+         res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
